9871fab9c568463c14b7ac9ed776b6d30f0a06ff
Israel-Yarbloom <i0504170922@gmail.com>
2026-07-06 14:29:52 +0300

    un-slesh the backend configuration

--- providers.tf
+++ providers.tf
@@ -1,11 +1,10 @@
 terraform {
   required_version = ">= 1.0.0"
 
-  # מושבת זמנית לטובת הבדיקות המקומיות שלכם
-  # backend "gcs" {
-  #   bucket = "terraform-mafat-ai-gee-monitor-dev"
-  #   prefix = "grafana-dashboards/state"
-  # }
+  backend "gcs" {
+    bucket = "terraform-mafat-ai-gee-monitor-dev"
+    prefix = "grafana-dashboards/state"
+  }
 
   required_providers {
     google = {
